dd9070344db2196024edbaa75f92edb30232fbd1
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-16 15:58:33 +0700

    feat: add router implementation for pages in app root file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ROUTES } from 'routes';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
+        {ROUTES.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.component />}
+          />
+        ))}
       </Routes>
     </Router>
   );
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,24 @@
 import React from 'react';
+import BagPage from 'pages/bag';
+import DetailsPage from 'pages/details';
+import HomePage from 'pages/home';
 
 export interface IRoute {
   path: string;
   component: React.FC;
-  exact: boolean;
 }
+
+export const ROUTES: IRoute[] = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/details/:pokemonId',
+    component: DetailsPage,
+  },
+  {
+    path: '/bag',
+    component: BagPage,
+  },
+];
