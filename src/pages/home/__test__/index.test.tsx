d58589dd620cc4bfc21d50f63cfbe644ac374d65
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-17 14:02:52 +0700

    feat: add header, search, list, navbar container to home page

--- src/pages/home/__test__/index.test.tsx
+++ src/pages/home/__test__/index.test.tsx
@@ -1,7 +1,21 @@
-import { render } from '@testing-library/react';
+import { ThemeProvider } from '@emotion/react';
+import { render, screen } from '@testing-library/react';
+import { THEME } from 'styles/theme';
 import HomePage from '..';
 
-it('should render home page', () => {
-  render(<HomePage />);
-  expect(<HomePage />).toBeTruthy();
+describe('home page', () => {
+  beforeEach(() => {
+    render(
+      <ThemeProvider theme={THEME.light}>
+        <HomePage />
+      </ThemeProvider>
+    );
+  });
+
+  it('should render header, search, list, and navbar component', () => {
+    expect(screen.getByTestId('header')).toBeTruthy();
+    expect(screen.getByTestId('search')).toBeTruthy();
+    expect(screen.getByTestId('list')).toBeTruthy();
+    expect(screen.getByTestId('navbar')).toBeTruthy();
+  });
 });
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,26 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { css, jsx } from '@emotion/react';
+import { css, jsx, useTheme } from '@emotion/react';
 import React from 'react';
 
 const HomePage: React.FC = () => {
+  const theme: any = useTheme();
+
   const styles = {
-    container: css``,
+    container: css`
+      background-color: ${theme.colors.background};
+      color: ${theme.colors.text};
+    `,
   };
-  return <div css={styles.container}>home works</div>;
+
+  return (
+    <div css={styles.container}>
+      <div data-testid="header">header</div>
+      <div data-testid="search">header</div>
+      <div data-testid="list">header</div>
+      <div data-testid="navbar">header</div>
+    </div>
+  );
 };
 
 export default HomePage;
