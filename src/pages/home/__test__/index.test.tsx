c4a61935bf8296ee69ae71af6bcb3454de92e746
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-17 14:33:35 +0700

    feat: add list name, query, and result card to list section

--- src/pages/home/__test__/index.test.tsx
+++ src/pages/home/__test__/index.test.tsx
@@ -20,8 +20,19 @@ describe('home page', () => {
     expect(screen.getByTestId('navbar')).toBeTruthy();
   });
 
-  it('header should have title and welcome message', () => {
+  it('header section should have title and welcome message', () => {
     expect(screen.getByTestId('header-title')).toBeTruthy();
     expect(screen.getByTestId('header-message')).toBeTruthy();
+  });
+
+  it('search section should have search bar and show all button', () => {
+    expect(screen.getByTestId('search-bar')).toBeTruthy();
+    expect(screen.getByTestId('search-all-btn')).toBeTruthy();
+  });
+
+  it('list section should have list name, query, and result cards', () => {
+    expect(screen.getByTestId('list-name')).toBeTruthy();
+    expect(screen.getByTestId('list-query')).toBeTruthy();
+    expect(screen.getAllByTestId('list-result-card')).toHaveLength(1);
   });
 });
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,11 +19,30 @@ const HomePage: React.FC = () => {
     <div css={styles.container}>
       <img src={cover} alt="cover image" data-testid="cover-img" />
       <div data-testid="header">
-        <h1 data-testid="header-title">title</h1>
-        <p data-testid="header-message">message</p>
+        <h1 data-testid="header-title">Hi, Welcome to Pokemon Sanctuary!</h1>
+        <p data-testid="header-message">
+          Find your favorite pokemons and make them as your beloved pets!
+        </p>
       </div>
-      <div data-testid="search">header</div>
-      <div data-testid="list">header</div>
+      <div data-testid="search">
+        <form data-testid="search-bar">
+          <input
+            type="text"
+            name="name"
+            id="pokemon-name"
+            placeholder="search by pokemon name.."
+          />
+          <button type="submit"></button>
+        </form>
+        <button data-testid="search-all-btn">show all</button>
+      </div>
+      <div data-testid="list">
+        <section>
+          <p data-testid="list-name">available pokemons</p>
+          <p data-testid="list-query">search: all pokemons</p>
+        </section>
+        <div data-testid="list-result-card">result card</div>
+      </div>
       <div data-testid="navbar">header</div>
     </div>
   );
