1b34358c47c98cfcf2df99e63bf2f24eee5963fe
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-18 09:20:48 +0700

    refactor: move interface models for theme into separate file

---
+++ src/styles/_base.ts
@@ -0,0 +1,35 @@
+import { AllThemeType, FontType } from './models';
+
+const FONTS: FontType = {
+  button: '0.6875rem',
+  body: '0.75rem',
+  headline: '1rem',
+  title: '1.5rem',
+};
+
+export const THEME: AllThemeType = {
+  light: {
+    colors: {
+      primary: '#245F71',
+      secondary: '#5BA3B3',
+      buttonBright: '#E1F5F6',
+      background: '#FFFFFF',
+      text: '#0F3E51',
+      grayed: '#BCBCBC',
+      delete: '#FB3232',
+    },
+    font: FONTS,
+  },
+  dark: {
+    colors: {
+      primary: '#245F71',
+      secondary: '#5BA3B3',
+      buttonBright: '#E1F5F6',
+      background: '#000000',
+      text: '#0F3E51',
+      grayed: '#BCBCBC',
+      delete: '#FB3232',
+    },
+    font: FONTS,
+  },
+};
---
+++ src/styles/models.ts
@@ -0,0 +1,28 @@
+export type ThemeSelections = 'light' | 'dark';
+
+type ColorsType = {
+  primary: string;
+  secondary: string;
+  buttonBright: string;
+  background: string;
+  text: string;
+  grayed: string;
+  delete: string;
+};
+
+export type FontType = {
+  button: string;
+  body: string;
+  headline: string;
+  title: string;
+};
+
+export type ThemeType = {
+  colors: ColorsType;
+  font: FontType;
+};
+
+export type AllThemeType = {
+  light: ThemeType;
+  dark: ThemeType;
+};
