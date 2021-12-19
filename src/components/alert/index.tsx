96925a580694bf4e8d65104e1ea38319b8d24f4e
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-19 10:58:42 +0700

    style: add style for alert component

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -12,7 +12,19 @@ const AlertMessage: React.FC<Props> = ({ msg, icon }) => {
   const theme: any = useTheme();
 
   const styles = {
-    container: css``,
+    container: css`
+      grid-row: span 3;
+      justify-self: center;
+      align-self: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    `,
+    icon: css`
+      height: 4rem;
+      width: 4rem;
+    `,
     msg: css`
       color: ${theme.colors.grayed};
       font-size: ${theme.fonts.body};
@@ -21,7 +33,7 @@ const AlertMessage: React.FC<Props> = ({ msg, icon }) => {
 
   return (
     <div css={styles.container}>
-      {icon && <img src={icon} alt="alert" />}
+      {icon && <img css={styles.icon} src={icon} alt="alert" />}
       <p css={styles.msg}>{msg}</p>
     </div>
   );
