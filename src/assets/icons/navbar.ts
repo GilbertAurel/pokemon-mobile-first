723c9b7649a227cc7853e723c7d1d57a470cd955
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-19 13:31:19 +0700

    chore: add menu buttons

---
+++ src/assets/icons/navbar.ts
@@ -0,0 +1,6 @@
+import bagActive from './bag-active.svg';
+import bag from './bag.svg';
+import homeActive from './home-active.svg';
+import home from './home.svg';
+
+export { home, homeActive, bag, bagActive };
