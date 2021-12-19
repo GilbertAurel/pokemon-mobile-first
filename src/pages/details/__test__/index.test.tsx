865574e69a0c1bcbc438543a4c35fddb6e674e01
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-12-19 19:05:07 +0700

    feat: implement get pokemon details custom hooks in details page

--- src/pages/details/__test__/index.test.tsx
+++ src/pages/details/__test__/index.test.tsx
@@ -1,7 +1,21 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { ThemeProvider } from '@emotion/react';
 import { render } from '@testing-library/react';
+import { THEME } from 'styles/_base';
 import DetailsPage from '..';
+import { GET_POKEMON_DETAIL_MOCKS_SUCCESS } from './mocks';
 
-it('should render home page', () => {
-  render(<DetailsPage />);
+it('should render details page', () => {
+  render(
+    <ThemeProvider theme={THEME.light}>
+      <MockedProvider
+        mocks={GET_POKEMON_DETAIL_MOCKS_SUCCESS}
+        addTypename={false}
+      >
+        <DetailsPage />
+      </MockedProvider>
+    </ThemeProvider>
+  );
+
   expect(<DetailsPage />).toBeTruthy();
 });
--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,13 +1,46 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { css, jsx } from '@emotion/react';
-import React from 'react';
+import { css, jsx, useTheme } from '@emotion/react';
+import { useGetPokemonDetails } from 'lib/useGetPokemonDetails';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const DetailsPage: React.FC = () => {
+  const theme: any = useTheme();
+  const params = useParams();
+  const [pokemon, loading, setPokemonName] = useGetPokemonDetails();
+
+  useEffect(() => {
+    if (params.pokemonName) {
+      setPokemonName(params.pokemonName);
+    }
+  }, []);
+
   const styles = {
-    container: css``,
+    wrapper: css`
+      height: 100vh;
+      width: 100vw;
+      background-color: ${theme.colors.grayed};
+    `,
+    container: css`
+      height: 100%;
+      max-width: 30rem;
+      margin: auto;
+      overflow-y: hidden;
+      background-color: ${theme.colors.background};
+    `,
   };
-  return <div css={styles.container}>details works!</div>;
+
+  return (
+    <div css={styles.wrapper}>
+      <div css={styles.container}>
+        <div>
+          <p>Pokemon Details</p>
+        </div>
+        {loading ? <p>loading</p> : <p>{pokemon?.name}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default DetailsPage;
